2de8b7e9ecfdd5c720a557f86700141e0043b225
Andi DeVois <adevois@gmail.com>
2017-06-05 19:06:05 -0500

    Setting up base files, adding angular, etc

--- Nightspotting/App_Start/BundleConfig.cs
+++ Nightspotting/App_Start/BundleConfig.cs
@@ -23,6 +23,10 @@ namespace Nightspotting
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                "~/Scripts/angular.js",
+                "~/Scripts/angular-route.js"));
         }
     }
 }
